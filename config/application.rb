8384d286636559efb42e6a5e3c196408bac0d224
fed1k <allamurotovcoder@mail.ru>
2022-07-26 00:48:09 +0500

    Add scaffold

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Project
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+  end
+end
---
+++ spec/views/foods/index.html.erb_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "foods/index", type: :view do
+  before(:each) do
+    assign(:foods, [
+      Food.create!(
+        name: "Name",
+        measurement_unit: "Measurement Unit",
+        price: 2,
+        quantity: 3,
+        user: nil
+      ),
+      Food.create!(
+        name: "Name",
+        measurement_unit: "Measurement Unit",
+        price: 2,
+        quantity: 3,
+        user: nil
+      )
+    ])
+  end
+
+  it "renders a list of foods" do
+    render
+    assert_select "tr>td", text: "Name".to_s, count: 2
+    assert_select "tr>td", text: "Measurement Unit".to_s, count: 2
+    assert_select "tr>td", text: 2.to_s, count: 2
+    assert_select "tr>td", text: 3.to_s, count: 2
+    assert_select "tr>td", text: nil.to_s, count: 2
+  end
+end
